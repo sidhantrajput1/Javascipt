154b16117772c2950e61a138d104fce91465456d
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-09 07:47:02 +0530

    conversion of data types

---
+++ lecture 2/conversionconfusion.js
@@ -0,0 +1,25 @@
+let score = "56"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN (Not a Number)
+// true => 1, false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "hitesh" => true
+// "" = false
