8f93796491788b4130f21bdcd61db660fa782614
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-10 10:21:56 +0530

    add string

---
+++ Lecture 3/strings.js
@@ -0,0 +1,36 @@
+
+/*
+Strings are for storing text
+Strings are written with quotes
+
+*/
+
+const firstName = "Sidhant"
+const lastName = " Singh"
+
+const fullName = firstName + lastName 
+console.log(`Hey EveryOne , I am ${fullName}`);
+
+const username = new String('sidhantrajput1')
+// console.log(username);
+// console.log(username[0]);
+// console.log(username.toUpperCase());
+// console.log(username.indexOf('t'));
+// console.log(username.charAt('6'));
+
+
+// const newString = username.substring(0, 5)
+// console.log(newString);
+
+// const anotherString = username.slice(-8, -4)
+// console.log(anotherString);
+
+
+// const email = "     sidhant@gmail.com    "
+// console.log(email);
+// console.log(email.trim());
+
+const url = "http://localhost:1234/user/refresh-token"
+console.log(url);
+// console.log(url.replace('/', '-'));
+console.log(url.includes('localhost'));
