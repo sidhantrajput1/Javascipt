97e5e87f64c15968ee89f870e82448d7acbb0e85
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-10 11:05:21 +0530

    Add Number and Math Datatypes

---
+++ Lecture 4/num_and_math.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score);
+// console.log(typeof score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2))
+// console.log(typeof balance);
+
+const num = 123.8966
+// console.log(num.toPrecision(3));
+
+const hunds = 100000
+// console.log(hunds.toLocaleString("en-In"))
+
+
+//++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-44));
+// console.log(Math.round(5.6))
+// console.log(Math.floor(5.9))
+// console.log(Math.ceil(5.2))
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
