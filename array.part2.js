b6786a62afb81272017b5b560b7c749066136a46
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-12 07:22:22 +0530

    add array part 2

---
+++ array.part2.js
@@ -0,0 +1,32 @@
+console.log("Jay Shree Krishna");
+
+const boys = ['kartik', 'Sidhant', 'Aadarsh' , "Rohit"]
+const girls = ['Supriya', 'Kajal', 'Ragini', 'Kriti']
+
+// boys.push(girls)
+// console.log(boys[3]);
+
+// console.log(boys[4][1]);
+
+
+// const pepole = boys.concat(girls)
+// console.log(pepole); // return new array
+
+// const humans = [...boys, ...girls]
+// console.log(humans);
+
+// const arr1 = [1, 2, 3, 4, [5, 6] , 6, ['Supriya', 'Kajal'], [8, 9, ['rohit', 'ragini']]]
+// const new_arr1 = arr1.flat(Infinity)
+// console.log(new_arr1);
+
+
+// console.log(Array.isArray("Sidhant"))
+// console.log(Array.from("Supriya"))
+
+// console.log(Array.from({name  : "Kartik"})) // interesting case
+
+let s1 = 100
+let s2 = 200
+let s3 = 300
+
+console.log(Array.of(s1, s2, s3))
