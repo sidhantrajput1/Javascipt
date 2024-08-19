d121eadddbcb7da343a714d170547eecca26cc0a
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-19 18:14:13 +0530

    add more in function

---
+++ function.js
@@ -0,0 +1,37 @@
+function square(number) {
+    return number * number;
+} 
+
+// console.log(square(6))
+
+// function myfunction(obj) {
+//     obj.make = 'Range Rover'
+// }
+
+// const myCar = {
+//     make : 'Land rover',
+//     model : 'classic',
+//     year : 1998
+// }
+
+// console.log(myCar.make)
+// myfunction(myCar);
+// console.log(myCar.make)
+
+// function myfunc(obj) {
+//     obj[0] = 30;
+// }
+
+// const arr = [45];
+
+// console.log(arr[0])
+// myfunc(arr)
+// console.log(arr[0])
+
+// ! Function Expression
+
+const square = function (number) {
+    return number * number;
+} 
+
+// console.log(square(6))
