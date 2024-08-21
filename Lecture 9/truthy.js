290c47f127f39f3335816f84318b76ff706d2aac
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-21 22:23:38 +0530

    add truthy conditional statement

---
+++ Lecture 9/truthy.js
@@ -0,0 +1,23 @@
+
+
+/*
+
+const userEmail = 'sidhant@gmail.com'
+
+if(userEmail) {
+    console.log('Got user Email')
+} else {
+    console.log("Don't have user email")
+}
+
+
+falsy values -> false, 0, -0, BigInt, 0n, "", null, undefiend, NaN
+
+truthy values -> true, "0", 'false', ' ', " ", [], {}, function(){}, 
+*/ 
+
+const userEmail = 'sidhant@gmail.com'
+
+if (userEmail.length === 0) {
+    console.log("Array is empty")
+}
