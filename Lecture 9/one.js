d6dc81b4e4f390a269bf6ee0dbcfbc2e2da337ae
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-21 22:05:27 +0530

    conditional statement added

---
+++ Lecture 9/one.js
@@ -0,0 +1,58 @@
+
+console.log('Jay Shree Krishan');
+
+const temperature = 45;
+
+// if(temperature > 50) {
+//     console.log('less then 50');
+// } else {
+//     console.log('temperature is greater than 50');
+// }
+
+
+// const score = 300
+
+// if (score > 200) {
+//     const power = 'fly';
+    // var power = 'fly'; // wrong input
+//     console.log(`user power : ${power}`)
+// }
+
+// console.log(`user power : ${power}`)
+
+const balance = 1000
+
+// if(balance > 500) console.log('Money'); // write way 
+// if(balance > 500) console.log('Money'), console.log('test'); // wrong way  
+
+// if (balance < 500) {
+//     console.log('less than 500')
+// } else if (balance < 750) {
+//     console.log('less than 750')
+// } else {
+//     console.log('less than 1200')
+// }
+
+
+
+/* 
+
+const userIsLoggedIn = true ;
+const debitCard = true ;
+
+if (userIsLoggedIn && debitCard) {
+    console.log("Allow to buy courses")
+} else {
+    console.log('User cannot buy courses')
+}
+
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+
+if (loggedInFromEmail || loggedInFromEmail) {
+    console.log('User Logged In')
+}
+
+*/
+
+
