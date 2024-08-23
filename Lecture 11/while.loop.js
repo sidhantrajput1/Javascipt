f7aa6d79fcf667ea7ca1fa945400c4eade86e1c5
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-23 14:08:28 +0530

    while and do while loop

---
+++ Lecture 11/while.loop.js
@@ -0,0 +1,30 @@
+/*
+
+while (condition) {
+    
+}
+
+et i = 0;
+while (i <= 10) {
+    console.log(`value of i is ${i}`)
+    i = i + 2;
+}
+
+const myArray = ['kartik', 'sidhant', 'manish', 'rohit', 'Himanshu'];
+let i = 0;
+
+while (i <= myArray.length) {
+    console.log(`value is ${myArray[i]}`)
+    i = i + 1;
+}
+
+*/
+
+// do whileloop
+
+let score = 1;
+
+do {
+    console.log(`Score id ${score}`)
+    score++;
+} while (score <= 10);
