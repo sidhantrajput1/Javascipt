e3d6d46388df0be316f0f4ac78bea306af438b6f
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-23 09:44:44 +0530

    updated truthy.js

--- Lecture 9/truthy.js
+++ Lecture 9/truthy.js
@@ -16,8 +16,25 @@ falsy values -> false, 0, -0, BigInt, 0n, "", null, undefiend, NaN
 truthy values -> true, "0", 'false', ' ', " ", [], {}, function(){}, 
 */ 
 
-const userEmail = 'sidhant@gmail.com'
+// const userEmail = 'sidhant@gmail.com'
 
-if (userEmail.length === 0) {
-    console.log("Array is empty")
-}
+// if (userEmail.length === 0) {
+//     console.log("Array is empty")
+// }
+
+
+let val1;
+
+// val1 = 10 ?? 20;
+// val1 = null ?? 20;
+// val1 = undefined ?? 15
+// val1 = undefined ?? 15 ?? 30
+// val1 = 40 ?? 15 ?? 30
+
+// console.log(val1);
+
+// Terniary Operator
+
+const icecreamPrice = 100;
+// icecreamPrice >= 80 ? console.log('Less than 80') : console.log('More than 100');
+icecreamPrice >= 150 ? console.log('Less than 100') : console.log('More than 100');
